625f6f78b06262f0cf8c9b8a2083055fd907af67
sinoso <tpgus123123@naver.com>
2025-04-19 18:13:22 +0900

    SlugTypographyStyle 클래스 data object로 변경

--- app/src/main/java/com/warehouseinhand/slug/ui/theme/SlugTypographyStyle.kt
+++ app/src/main/java/com/warehouseinhand/slug/ui/theme/SlugTypographyStyle.kt
@@ -5,15 +5,196 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-object SlugTypographyStyle {
+data object SlugTypographyStyle {
+    /**
+     * fontSize = 56.sp,
+     * lineHeight = 68.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val Display: TextStyle = TextStyle(
+        fontSize = 56.sp,
+        lineHeight = 68.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
 
     /**
-     *         fontSize = 16.sp,
-     *         lineHeight = 22.sp,
-     *         fontWeight = FontWeight.Normal,
-     *         fontFamily = pretendardFontFamily
-     *         */
+     * fontSize = 32.sp,
+     * lineHeight = 40.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val HeadingLargeBold: TextStyle = TextStyle(
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 28.sp,
+     * lineHeight = 36.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val HeadingMediumBold: TextStyle = TextStyle(
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 24.sp,
+     * lineHeight = 32.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val HeadingSmallBold: TextStyle = TextStyle(
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 22.sp,
+     * lineHeight = 30.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val HeadingMiniBold: TextStyle = TextStyle(
+        fontSize = 22.sp,
+        lineHeight = 30.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 20.sp,
+     * lineHeight = 26.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val TitleLargeBold: TextStyle = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 20.sp,
+     * lineHeight = 26.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val TitleLargeMedium: TextStyle = TextStyle(
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 18.sp,
+     * lineHeight = 24.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val TitleMediumBold: TextStyle = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 18.sp,
+     * lineHeight = 24.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val TitleMediumMedium: TextStyle = TextStyle(
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 17.sp,
+     * lineHeight = 24.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyLargeBold: TextStyle = TextStyle(
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 17.sp,
+     * lineHeight = 24.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyLargeMedium: TextStyle = TextStyle(
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 17.sp,
+     * lineHeight = 24.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyLargeRegular: TextStyle = TextStyle(
+        fontSize = 17.sp,
+        lineHeight = 24.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 16.sp,
+     * lineHeight = 22.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMediumBold: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 16.sp,
+     * lineHeight = 22.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
     val BodyMediumMedium: TextStyle = TextStyle(
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 16.sp,
+     * lineHeight = 22.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMediumRegular: TextStyle = TextStyle(
         fontSize = 16.sp,
         lineHeight = 22.sp,
         fontWeight = FontWeight.Normal,
@@ -21,13 +202,184 @@ object SlugTypographyStyle {
     )
 
     /**
-     *         fontSize = 14.sp,
-     *         fontWeight = FontWeight.Normal,
-     *         fontFamily = pretendardFontFamily
-     *         */
-    val LoginSubTitle: TextStyle = TextStyle(
-        fontSize = 14.sp,
+     * fontSize = 15.sp,
+     * lineHeight = 20.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodySmallBold: TextStyle = TextStyle(
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 15.sp,
+     * lineHeight = 20.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodySmallMedium: TextStyle = TextStyle(
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 15.sp,
+     * lineHeight = 20.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodySmallRegular: TextStyle = TextStyle(
+        fontSize = 15.sp,
+        lineHeight = 20.sp,
         fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 14.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMiniBold: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 14.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMiniMedium: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 14.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMiniRegular: TextStyle = TextStyle(
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 13.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMicroBold: TextStyle = TextStyle(
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 13.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMicroMedium: TextStyle = TextStyle(
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 13.sp,
+     * lineHeight = 18.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyMicroRegular: TextStyle = TextStyle(
+        fontSize = 13.sp,
+        lineHeight = 18.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 12.sp,
+     * lineHeight = 16.sp,
+     * fontWeight = FontWeight.Bold,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyTinyBold: TextStyle = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Bold,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 12.sp,
+     * lineHeight = 16.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyTinyMedium: TextStyle = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 12.sp,
+     * lineHeight = 16.sp,
+     * fontWeight = FontWeight.Normal,
+     * fontFamily = pretendardFontFamily
+     */
+    val BodyTinyRegular: TextStyle = TextStyle(
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 11.sp,
+     * lineHeight = 14.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val CaptionLargeMedium: TextStyle = TextStyle(
+        fontSize = 11.sp,
+        lineHeight = 14.sp,
+        fontWeight = FontWeight.Medium,
+        fontFamily = pretendardFontFamily
+    )
+
+    /**
+     * fontSize = 10.sp,
+     * lineHeight = 14.sp,
+     * fontWeight = FontWeight.Medium,
+     * fontFamily = pretendardFontFamily
+     */
+    val CaptionMediumMedium: TextStyle = TextStyle(
+        fontSize = 10.sp,
+        lineHeight = 14.sp,
+        fontWeight = FontWeight.Medium,
         fontFamily = pretendardFontFamily
     )
 
