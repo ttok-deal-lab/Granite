1c4cda4d1f3cbfd683ab83385d1724d0b0241b38
sinoso <tpgus123123@naver.com>
2025-09-09 00:37:10 +0900

    modifier compose 버전에 맞춰 최신화

--- app/src/main/java/com/warehouseinhand/slug/util/CustomModifiers.kt
+++ app/src/main/java/com/warehouseinhand/slug/util/CustomModifiers.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
@@ -50,7 +50,7 @@ fun Modifier.blockingClickableWithRipple(
     blockingClickable(
         onClick = onClick,
         enable = enable,
-        indication = rememberRipple(),
+        indication = ripple(),
         interactionSource = interactionSource,
         eventLimitMillisecond = eventLimitMillisecond
     )
