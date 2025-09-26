142882395217cc759ca2e5dff033a65bfbfa31cd
sinoso <tpgus123123@naver.com>
2025-09-26 14:47:15 +0900

    fix: favorite 목록 화면 전체 재구성때만 요청하게 수정

--- app/src/main/java/com/warehouseinhand/slug/favorite/FavoriteRoute.kt
+++ app/src/main/java/com/warehouseinhand/slug/favorite/FavoriteRoute.kt
@@ -2,6 +2,7 @@ package com.warehouseinhand.slug.favorite
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
@@ -21,7 +22,7 @@ internal fun FavoriteRoute(
     val onItemClicked: (ProductItemUiModel) -> Unit = {}
     val onNotificationClick: () -> Unit = {}
     //TODO : 아래 영역에서 favorite 버튼 눌러도 사라지지 않게 만들고, 상세화면에서만 해제 가능하게 할것!
-    SideEffect {
+    LaunchedEffect(Unit) {
         viewModel.requestFavoriteList()
     }
     FavoriteScreen(
