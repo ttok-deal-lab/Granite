824067f29a311420f91d64da517bc672cef56e81
sinoso <tpgus123123@naver.com>
2025-10-23 23:59:55 +0900

    fix : 미사용 import 제거

--- app/src/main/java/com/warehouseinhand/slug/ui/component/tab/LineHugTab.kt
+++ app/src/main/java/com/warehouseinhand/slug/ui/component/tab/LineHugTab.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.warehouseinhand.slug.ui.theme.NeutralSubtler
