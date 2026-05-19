57203c7a19ec66b601949ba3591fe926665ce34f
sinoso <tpgus123123@naver.com>
2026-05-19 22:40:36 +0900

    Update LocationSelectorContent.kt

--- app/src/main/java/com/warehouseinhand/slug/home/bottomsheet/location/LocationSelectorContent.kt
+++ app/src/main/java/com/warehouseinhand/slug/home/bottomsheet/location/LocationSelectorContent.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
@@ -27,6 +28,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -126,7 +128,7 @@ fun LocationSelectorList(
                         onClick = { onMainClicked(locationMain) })
                 }
             }
-
+            Spacer(Modifier.width(2.dp).background(Color.White))//TODO : 다크모드 처리필요함!
             LazyColumn(
                 modifier = Modifier.weight(1f), state = subListState
             ) {
